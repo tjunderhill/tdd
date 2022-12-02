6aa818f2fa1c7ebf07283a27e7a97ed0ea48c97d
Adrian <ajanuzi@confluent.io>
2022-12-02 13:30:42 +0000

    Extra password tests

--- src/main/java/org/example/PasswordValidator.java
+++ src/main/java/org/example/PasswordValidator.java
@@ -6,10 +6,32 @@ public class PasswordValidator {
   }
 
   public boolean passwordIsValid(String password) {
-    if (password.length() > 8){
-      return true;
-    } else {
+    if (!checkPasswordLength(password)) {
       return false;
     }
+
+    if (!containsAtLeastOneNumber(password)) {
+      return false;
+    }
+
+    return true;
+  }
+
+
+  private boolean containsAtLeastOneNumber(String password) {
+    if (!password.matches(".*\\d.*")) {
+      return false;
+    }
+
+    return true;
+  }
+
+
+  private boolean checkPasswordLength(String password) {
+    if (!(password.length() > 8)) {
+      return false;
+    }
+
+    return true;
   }
 }
--- src/test/java/org/example/PasswordValidatorTest.java
+++ src/test/java/org/example/PasswordValidatorTest.java
@@ -11,6 +11,7 @@ public class PasswordValidatorTest {
      * - Password Validator. 
      * - Write a function that takes a password as a string  and returns true if the password meets certain criteria.
      *  - The password needs to have more than 8 characters
+     *  - At least 1 number
      */
 
     @Test
@@ -36,5 +37,32 @@ public class PasswordValidatorTest {
         PasswordValidator passwordValidator = new PasswordValidator();
 
         assertFalse(passwordValidator.passwordIsValid("Password"));
+    }
+
+    @Test
+    public void passwordContainsAtLeastOneNumber() {
+        // this
+        PasswordValidator passwordValidator = new PasswordValidator();
+
+        assertTrue(passwordValidator.passwordIsValid("password1"));
+    }
+
+
+    @Test
+    public void passwordContainsNoNumber() {
+        // this
+        PasswordValidator passwordValidator = new PasswordValidator();
+
+        assertFalse(passwordValidator.passwordIsValid("passworda"));
+    }
+
+
+    @Test
+    public void something(){
+        //this
+        PasswordValidator passwordValidator = new PasswordValidator();
+
+        //when
+        assertFalse(passwordValidator.passwordIsValid("Passworda"));
     }
 }
